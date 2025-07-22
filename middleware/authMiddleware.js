8e7873f64d8f3f28f01aa3facb9b404bc64bca53
chintala naga venkata ajay kumar <chnvakumarofficial@gmail.com>
2025-07-22 10:54:55 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,14 +1,18 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.header('Authorization');
+  const authHeader = req.header('Authorization');
 
-  if (!token)
+  if (!authHeader)
     return res.status(401).json({ message: "No token provided" });
+
+  const token = authHeader.startsWith('Bearer ')
+    ? authHeader.split(' ')[1]
+    : authHeader;
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // now contains `id`
+    req.user = decoded; // Now contains `id`
     next();
   } catch (err) {
     return res.status(401).json({ message: "Invalid token" });
